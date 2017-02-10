88aac6a72bfbbd714d5ccc598bd87b51e5a57e9a
HSaturn <hsaturn@gmail.com>
2017-02-10 13:09:09 +0100

    Add modl

---
+++ core/model/Model.hpp
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Model.hpp
+ * Author: hsaturn
+ *
+ * Created on 3 février 2017, 00:44
+ */
+
+#ifndef MODEL_HPP
+#define MODEL_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+#include "tiny_obj_loader.h"
+#include <glm.hpp>
+
+using namespace std;
+
+class Model
+{
+
+	
+	public:
+		
+		static const Model* get(const string& name);
+		void renderWireFrame() const;
+		void render() const;
+		
+		// Bounding box
+		const glm::vec3& getMinCoord() const { return bmin; }
+		const glm::vec3& getMaxCoord() const { return bmax; }
+		
+	private:
+		Model();
+		~Model();
+		Model(const Model&);
+		Model& operator=(const Model&);
+		
+		bool loadObjAndConvert(const string&);
+
+		typedef struct {
+		  GLuint vb;  // vertex buffer FIXME opengl memory leak
+		  int numTriangles;
+		  size_t material_id;
+		} DrawObject;
+		
+		/*
+		std::vector< glm::vec3 > vertices;
+		
+		static map<const string, Model*>	models;
+		GLuint vbo_id;
+		*/
+		
+		glm::vec3 bmin;
+		glm::vec3 bmax;
+		std::vector<DrawObject> drawObjects;
+		
+	private: // Static members
+		static std::vector<tinyobj::material_t> materials;
+		static std::map<std::string, GLuint> textures;
+		static map<const string, Model*>	models;
+		
+		void main();
+		
+};
+
+#endif /* MODEL_HPP */
+
