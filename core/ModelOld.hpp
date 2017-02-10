5fecc6d16cf444091698423919a32d7180f976e4
hsaturn <hsaturn@gmail.com>
2017-02-10 13:01:26 +0100

    Missing

---
+++ core/ModelOld.hpp
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Model.hpp
+ * Author: hsaturn
+ *
+ * Created on 3 février 2017, 00:44
+ */
+
+#ifndef MODELOLD_HPP
+#define MODELOLD_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+
+using namespace std;
+
+class ModelOld
+{
+	public:
+		
+		static const ModelOld* get(const string& name);
+		void render() const;
+		
+	private:
+		ModelOld();
+		~ModelOld();
+		ModelOld(const ModelOld&);
+		ModelOld& operator=(const ModelOld&);
+		
+		bool load(const string&);
+		
+		std::vector< glm::vec3 > vertices;
+		
+		static map<const string, ModelOld*>	models;
+		GLuint vbo_id;
+		
+};
+
+#endif /* MODEL_HPP */
+
