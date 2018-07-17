4a88d1df79399882ff3992aa029e8dbcf0d2a016
hsaturn <hsaturn@gmail.com>
2018-07-17 23:23:00 +0200

    Added application core

---
+++ core/application/Renderable.cpp
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Renderable.cpp
+ * Author: hsaturn
+ * 
+ * Created on 17 août 2017, 08:27
+ */
+
+#include "Renderable.hpp"
+
+Renderable::Renderable() { }
+
+Renderable::Renderable(const Renderable& orig) { }
+
+Renderable::~Renderable() { }
+
